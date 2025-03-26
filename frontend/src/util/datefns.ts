5f71a3feb58b4995a658fa71854fb48b8f21e579
Fasalulla K P <fasalkp26@gmail.com>
2025-03-27 02:58:49 +0530

    Refactored code for improved readability and maintainability

--- frontend/src/util/datefns.ts
+++ frontend/src/util/datefns.ts
@@ -1,6 +1,7 @@
 // Convert ISO time to human-readable format
 export const formatTime = (isoString: Date | undefined) => {
     if(!isoString) return
+    
     const date = new Date(isoString);
     return date.toLocaleString("en-US", {
         hour: "numeric",
